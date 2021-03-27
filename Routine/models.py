d088d79e63b1a4dcdd816a97e7be4075348cfe73
Masnun-13 <masnun111112@gmail.com>
2021-03-28 00:07:15 +0600

    database complete?

--- Routine/models.py
+++ Routine/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Routine(models.Model):
+    routine_user = models.CharField(max_length=20, db_column="User")
+    routine_time = models.IntegerField(db_column="Time")
+    routine_activity = models.CharField(max_length=50, db_column="Activity")
+
+    class Meta:
+        db_table="Routine"
+
--- User/models.py
+++ User/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Userinfo(models.Model):
+    user_name = models.CharField(max_length=20, db_column="Username")
+    user_age = models.IntegerField(db_column="Age")
+    user_occupation = models.CharField(max_length=50, db_column="Occupation")
+
+    class Meta:
+        db_table = "User Info"
+
